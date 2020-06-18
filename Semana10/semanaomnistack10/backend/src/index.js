7a0959ccff47174bfcdfcbaf36d523667a950fef
RenatoHSASilva <renatohenriquesesso@hotmail.com>
2020-06-17 22:42:21 -0300

    Adicionando a Semana 10

---
+++ Semana10/semanaomnistack10/backend/src/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const http = require('http');
+const routes = require('./routes');
+const { setupWebsocket } = require('./websocket')
+
+const app = express();
+const server = http.Server(app);
+
+setupWebsocket(server);
+
+mongoose.connect('mongodb+srv://renato:123@cluster0-gmhbl.mongodb.net/week10?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+//Método HTTP: GET, POST, PUT, DELETE
+
+//Tipos de parametros:
+
+//Query Params: request.query (Filtros, Ordenação, paginação, ...)
+//Route Params: request.params (Indentificar um recurso na alterção ou remoção)
+//Body: request.body (Dados para criação ou alteração de um registro)
+
+//MongoDB (Não-relacional)
+
+server.listen(3333);
